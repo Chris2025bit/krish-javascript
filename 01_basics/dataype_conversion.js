429565f337056b024114a64f12d4fb05913e584b
Chris2025bit <mahtochris@gmail.com>
2025-09-15 17:48:18 +0000

    conversion of datatype done

---
+++ 01_basics/dataype_conversion.js
@@ -0,0 +1,30 @@
+let score = "45"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "45" => 45
+// "45abc" => NaN
+// null => 0
+// "krish" => NaN
+// true => 1; false => 0
+ let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "krish" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+
