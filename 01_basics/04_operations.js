c74932a68b5cf97fa75a512789938ae93c57a257
Chris2025bit <mahtochris@gmail.com>
2025-09-16 11:31:28 +0000

    operations done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " krish"
+let str3 = str1 + str2
+
+console.log(str3);
+
+// checking some operation
+
+console.log(2 + "3");
+console.log("2" + 3)
+console.log("1" + 2 + 3)
+console.log(1 + "2" + 3)
+console.log(1 + 2 + "3");
+
+//if first is string then all are treated as string
+
+// console.log(+true) is supported resulting in 1 but its confusing and not usable
+// console.log(true+) is not supported
+// console.log(+"") resulting in 0 but not usable
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// upper code resulting in 4 but not usable
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
